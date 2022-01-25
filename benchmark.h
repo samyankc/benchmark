e4b099ea15ca5dab8c05e6dcd0f71de45dcec36e
samyankc <80664080+samyankc@users.noreply.github.com>
2022-01-26 02:40:36 +0800

    Add files via upload

--- benchmark.h
+++ benchmark.h
@@ -9,36 +9,57 @@
 
 #include <chrono>
 #include <iostream>
+#include <string>
+#include <vector>
 
 namespace {
 
 struct BenchmarkResult
 {
+    std::string Title;
     std::size_t TotalCycle;
-    std::size_t Rep;
+    std::size_t TotalIteration;
 
     friend auto& operator<<( auto& out, const BenchmarkResult& Result )
     {
-        auto Latency    = Result.TotalCycle / Result.Rep;
-        auto Throughput = 1000000000 * Result.Rep / Result.TotalCycle;
+        auto Latency    = Result.TotalCycle / Result.TotalIteration;
+        auto Throughput = 1000000000 * Result.TotalIteration / Result.TotalCycle;
         return out << "\n    Latency : " << Latency << " Cycles"            //
                    << "\n Throughput : " << Throughput << " / Giga Cycles"  //
                    << std::endl;
     }
 };
 
+static auto BenchmarkResults = std::vector<BenchmarkResult>{};
+
+struct BenchmarkAnalyzerType
+{
+    BenchmarkAnalyzer() { BenchmarkResults.reserve( 10 ); }
+    ~BenchmarkAnalyzerType()
+    {
+        std::cout << "\t\tLatency\t\tThroughput\n";
+        for( auto&& Result : BenchmarkResults )
+        {
+            auto Latency    = Result.TotalCycle / Result.TotalIteration;
+            auto Throughput = 1000000000 * Result.TotalIteration / Result.TotalCycle;
+            std::cout << Result.Title << "\t"  //
+                      << Latency << "\t\t"     //
+                      << Throughput << "\n";
+        }
+    }
+};
+
+static auto BenchmarkAnalyzer = BenchmarkAnalyzerType{};
+
 struct BenchmarkContainer
 {
-    using clock         = std::chrono::steady_clock;
-    using time_point    = std::chrono::time_point<clock>;
-    using time_interval = std::chrono::milliseconds;
+    using clock      = std::chrono::steady_clock;
+    using time_point = std::chrono::time_point<clock>;
 
-    constexpr static time_interval MaxDuration{ 2000 };
-    constexpr static std::size_t MaxRep{ 50000 };
+    constexpr static auto MaxDuration  = std::chrono::milliseconds{ 2000 };
+    constexpr static auto MaxIteration = std::size_t{ 50000 };
 
-    inline static BenchmarkResult SharedResult{};
-
-    BenchmarkResult& Result{ SharedResult };
+    BenchmarkResult& Result;
 
     struct Sentinel
     {};
@@ -48,25 +69,24 @@ struct BenchmarkContainer
     {
         BaseRange& Base;
         time_point EndTime;
-        std::size_t RemainRep;
+        std::size_t RemainIteration;
         std::size_t StartCycle;
 
         auto operator*() { return 0; }
-        auto operator++() { --RemainRep; }
-        auto operator!=( Sentinel ) { return RemainRep > 0 && clock::now() < EndTime; }
+        auto operator++() { --RemainIteration; }
+        auto operator!=( Sentinel ) { return RemainIteration > 0 && clock::now() < EndTime; }
 
         Iterator( BaseRange& Base )
             : Base{ Base },                                      //
               EndTime{ clock::now() + BaseRange::MaxDuration },  //
-              RemainRep{ BaseRange::MaxRep },                    //
+              RemainIteration{ BaseRange::MaxIteration },        //
               StartCycle{ __rdtsc() }
         {}
 
         ~Iterator()
         {
-            Base.Result.TotalCycle = __rdtsc() - StartCycle;
-            Base.Result.Rep        = BaseRange::MaxRep - RemainRep;
-            if( &Base.Result == &BaseRange::SharedResult ) std::cout << Base.Result;
+            Base.Result.TotalCycle     = __rdtsc() - StartCycle;
+            Base.Result.TotalIteration = BaseRange::MaxIteration - RemainIteration;
         }
     };
 
@@ -79,8 +99,12 @@ struct BenchmarkContainer
     }
 };
 
-};  // namespace
+auto Benchmark( std::string&& BenchmarkTitle )
+{
+    BenchmarkResults.emplace_back( std::move( BenchmarkTitle ), 0, 0 );
+    return BenchmarkContainer{ BenchmarkResults.back() };
+}
 
-static auto Benchmark = BenchmarkContainer{};
+};  // namespace
 
 #endif /* BENCHMARK_H */
